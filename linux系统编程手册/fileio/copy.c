ef0067c76fe4e27ce8f249b72c761b6682425626
zzu-andrew <564631192@qq.com>
2019-01-22 23:45:02 +0800

    添加makefile文件

--- linux系统编程手册/fileio/copy.c
+++ linux系统编程手册/fileio/copy.c
@@ -1,5 +1,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 
 
@@ -17,33 +21,33 @@ main(int argc, char *argv[])
     char buf[BUF_SIZE];
 
     if (argc != 3 || strcmp(argv[1], "--help") == 0)
-        usageErr("%s old-file new-file\n", argv[0]);
+        printf("%s old-file new-file\n", argv[0]);
 
     /* Open input and output files */
 
     inputFd = open(argv[1], O_RDONLY);
     if (inputFd == -1)
-        errExit("opening file %s", argv[1]);
+        printf("opening file %s", argv[1]);
 
     openFlags = O_CREAT | O_WRONLY | O_TRUNC;
     filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
                 S_IROTH | S_IWOTH;      /* rw-rw-rw- */
     outputFd = open(argv[2], openFlags, filePerms);
     if (outputFd == -1)
-        errExit("opening file %s", argv[2]);
+        printf("opening file %s", argv[2]);
 
     /* Transfer data until we encounter end of input or an error */
 
     while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0)
         if (write(outputFd, buf, numRead) != numRead)
-            fatal("couldn't write whole buffer");
+            printf("couldn't write whole buffer");
     if (numRead == -1)
-        errExit("read");
+        printf("read");
 
     if (close(inputFd) == -1)
-        errExit("close input");
+        printf("close input");
     if (close(outputFd) == -1)
-        errExit("close output");
+        printf("close output");
 
     exit(EXIT_SUCCESS);
 }
