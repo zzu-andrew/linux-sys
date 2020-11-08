abf3a864b7d4e2a84cceb3062bd2c436f0b84f73
Achou.Wang <564631192@qq.com>
2020-11-09 00:20:05 +0800

    取出编译生成文件

--- CMake/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /work/linux-sys/CMake
-# Build directory: /work/linux-sys/CMake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
