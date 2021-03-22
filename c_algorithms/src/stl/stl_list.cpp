b70ac5295afeaf7154e62b365f9c39180fb4c4ca
zzu-andrew <564631192@qq.com>
2021-03-22 22:20:30 +0800

    添加list

---
+++ c_algorithms/src/stl/stl_list.cpp
@@ -0,0 +1,34 @@
+//
+// Created by andrew on 2021/3/22.
+//
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+/*
+ * list 是一个双向链表容器，可以高效进行数据的插入和删除操作
+ * list 不可以随机存取数据(因为是链表)，所以不支持at.(pos)函数和[]操作符 可以it++但是不能it+5
+ * */
+
+/*
+ *  list指定位置插入元素的含义
+ *
+ *  list删除操作是 [0, 3)左闭又开的
+ * */
+
+void list_init() {
+    list<int> l;
+
+
+
+}
+
+int main(int argc, char* argv[]) {
+
+
+
+
+
+    return 0;
+}
