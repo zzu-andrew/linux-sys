d94c9cc38b814ddd66b13810bb779d9576066bd4
zzu-andrew <564631192@qq.com>
2021-03-23 22:45:48 +0800

    添加默认优先级队列

---
+++ c_algorithms/src/stl/stl_priority_queue.cpp
@@ -0,0 +1,22 @@
+//
+// Created by andrew on 2021/3/23.
+//
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+void priority_queue_init() {
+    priority_queue<int> q1; // 默认是情况下是最大优先级队列
+
+
+}
+
+
+int main(int argc, char* argv[]) {
+
+
+    priority_queue_init();
+
+    return 0;
+}
